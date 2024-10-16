752394ad886ca2c216e50141876388ae21311468
Yanya07 <yankabumoney@gmail.com>
2024-10-16 12:39:57 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.taskTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.completeButton = new System.Windows.Forms.Button();
+            this.taskListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // taskTextBox
+            // 
+            this.taskTextBox.Location = new System.Drawing.Point(12, 12);
+            this.taskTextBox.Name = "taskTextBox";
+            this.taskTextBox.Size = new System.Drawing.Size(300, 22);
+            this.taskTextBox.TabIndex = 0;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(318, 11);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Добавить";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(12, 220);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(75, 23);
+            this.removeButton.TabIndex = 2;
+            this.removeButton.Text = "Удалить";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // completeButton
+            // 
+            this.completeButton.Location = new System.Drawing.Point(93, 220);
+            this.completeButton.Name = "completeButton";
+            this.completeButton.Size = new System.Drawing.Size(150, 23);
+            this.completeButton.TabIndex = 3;
+            this.completeButton.Text = "Отметить как выполнено";
+            this.completeButton.UseVisualStyleBackColor = true;
+            this.completeButton.Click += new System.EventHandler(this.completeButton_Click);
+            // 
+            // taskListBox
+            // 
+            this.taskListBox.FormattingEnabled = true;
+            this.taskListBox.ItemHeight = 16;
+            this.taskListBox.Location = new System.Drawing.Point(12, 50);
+            this.taskListBox.Name = "taskListBox";
+            this.taskListBox.Size = new System.Drawing.Size(381, 164);
+            this.taskListBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 260);
+            this.Controls.Add(this.taskListBox);
+            this.Controls.Add(this.completeButton);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.taskTextBox);
+            this.Name = "Form1";
+            this.Text = "Учет задач";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox taskTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button completeButton;
+        private System.Windows.Forms.ListBox taskListBox;
+    }
+}
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            // Добавление задачи из текстового поля в список
+            string task = taskTextBox.Text.Trim();
+            if (!string.IsNullOrEmpty(task))
+            {
+                taskListBox.Items.Add(task);
+                taskTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите задачу.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            // Удаление выбранной задачи из списка
+            if (taskListBox.SelectedItem != null)
+            {
+                taskListBox.Items.Remove(taskListBox.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, выберите задачу для удаления.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void completeButton_Click(object sender, EventArgs e)
+        {
+            // Отметка задачи как выполненной
+            if (taskListBox.SelectedItem != null)
+            {
+                string completedTask = taskListBox.SelectedItem.ToString();
+                taskListBox.Items[taskListBox.SelectedIndex] = completedTask + " (Выполнено)";
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, выберите задачу для отметки как выполненную.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
